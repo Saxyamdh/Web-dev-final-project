cae4aec1b3c88b5332a8a311ad5a78403aec0115
Sachyam Dhungana <97268770+Saxyamdh@users.noreply.github.com>
2023-04-17 10:29:30 +0545

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect,request
 
 app = Flask(__name__)
 
@@ -9,6 +9,10 @@ def index():
 @app.route('/home')
 def home():
     return render_template('home.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run()
